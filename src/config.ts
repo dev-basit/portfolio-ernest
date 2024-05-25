095a7bf7d6b759267b83c6beaed22fce20373b35
Abdul Basit <dev.basit2020@gmail.com>
2024-05-25 19:16:17 +0500

    Added content for About section

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,9 @@ export const config = {
   name: name,
   nameTagLine: `My name is ${name}.`,
   descriptionTagLine: "I aspire to solve real-world problems through innovation and data-driven design.",
+
+  aboutText:
+    "I am currently a second year undergraduate studying Information Systems (Business Analytics) and Finance at Singapore Management University. I am passionate about visual and information design, and enjoy working with design/analytics tools such as Figma, Canva, PowerPoint and Tableau to create compelling visuals that communicate amazing ideas.",
+  aboutText2:
+    "I am driven by my passion towards technology, innovation, good design, a hunger for continuous learning, and above all, my Yakun Kopi Bing 大杯 (Large Iced Nanyang-style Coffee).",
 };
