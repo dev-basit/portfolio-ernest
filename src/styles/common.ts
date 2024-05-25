404ad7cee2e482e0378bd30a44d360772eb8c7e5
Abdul Basit <dev.basit2020@gmail.com>
2024-05-25 19:15:45 +0500

    Added text styled-component

--- src/styles/common.ts
+++ src/styles/common.ts
@@ -2,4 +2,12 @@ import styled from "styled-components";
 
 const Anchor = styled.a``;
 
-export const CommonStyles = { Anchor };
+const Text = styled.p`
+  font-size: 2rem;
+
+  @media screen and (max-width: 576px) {
+    font-size: 1rem;
+  }
+`;
+
+export const CommonStyles = { Anchor, Text };
