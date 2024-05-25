1bcfdaf735f787b233a1feddbd4da32daef87157
Abdul Basit <dev.basit2020@gmail.com>
2024-05-25 15:55:13 +0500

    Added central place for content

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+const name = "Earnest";
+
+export const config = {
+  name: name,
+  nameTagLine: `My name is ${name}.`,
+  descriptionTagLine: "I aspire to solve real-world problems through innovation and data-driven design.",
+};
