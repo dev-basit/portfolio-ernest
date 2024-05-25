880d92c1ebc2703e101b34d1c93f0736ea10125c
Abdul Basit <dev.basit2020@gmail.com>
2024-05-26 01:15:40 +0500

    fontSize will be recieved from props

--- src/styles/common.ts
+++ src/styles/common.ts
@@ -2,18 +2,29 @@ import styled from "styled-components";
 
 const Anchor = styled.a``;
 
+const ContainerHeading = styled.h1`
+  text-align: center;
+  margin: 3rem 0;
+  color: ${({ color }) => color || "white"};
+  letter-spacing: 0.5rem;
+
+  @media screen and (max-width: 768px) {
+    margin-bottom: 3rem;
+  }
+`;
+
 const Text = styled.p`
-  font-size: 2.5rem;
+  font-size: ${(fonts) => fonts.lg || "2.5rem"};
 
   @media screen and (max-width: 1280px) {
-    font-size: 2rem;
+    font-size: ${(fonts) => fonts.md || "2rem"};
   }
 
   @media screen and (max-width: 576px) {
-    font-size: 1.5rem;
+    font-size: ${(fonts) => fonts.sm || "1.5rem"};
   }
 `;
 
 const HorizontalBreak = styled.br``;
 
-export const CommonStyles = { Anchor, Text, HorizontalBreak };
+export const CommonStyles = { Anchor, ContainerHeading, Text, HorizontalBreak };
